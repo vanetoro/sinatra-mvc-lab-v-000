be3f535bf8e7db4852086e4a352b4100dfbaed9d
Vanessa Toro <vanetoro84@gmail.com>
2018-05-22 18:20:43 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,10 +4,13 @@ class PigLatinizer
   def piglatinize(text)
     @text = text.downcase
     text = text.split()
+    if text = 'I'
+      'Iway'
+    else
     text.map do |word|
       split = word.split(//,2)
       "#{split[1]}#{split[0].downcase}ay"
      end.join
   end
-
+end
 end
