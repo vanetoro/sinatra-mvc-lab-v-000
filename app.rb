d95285b15f672460ff59a741dc77bc2a1acac1db
Vanessa Toro <vanetoro84@gmail.com>
2018-05-22 19:03:11 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,12 +1,17 @@
 require_relative 'config/environment'
+require 'pry'
+
 
 class App < Sinatra::Base
 
-  get '/user_input' do
+  get '/' do
     erb :user_input
   end
 
-  POST '/user_input' do
+  post '/piglatinize' do
+   @text = Piglatinizer.new(params[:name])
+   binding.pry
+    erb :piglatinize
 
   end
 end
--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,14 +1,22 @@
-class Piglatinizer
+class PigLatinizer
     attr_reader :text
-  def intialize(text)
-    @text = text
-  end
 
-  def to_pig_latin
-    phrase = text.split()
-    phrase.map do |word|
-      split = word.split(//,2)
-      "#{split[1]}#{split[0].downcase}ay "
-     end.join
-  end
+  def piglatinize(text)
+    @text = text.downcase
+    text = text.split()
+
+      text.map do |word|
+        if word.length < 1
+          binding.pry
+        else
+          split = word.split(/([aeiou])/,2)
+          if split[0].empty?
+            "#{split[2]}#{split[1].downcase}way"
+
+            else
+            "#{split[1]}#{split[2]}#{split[0].downcase}ay"
+          end.flatten
+        end
+      end
+   end
 end
