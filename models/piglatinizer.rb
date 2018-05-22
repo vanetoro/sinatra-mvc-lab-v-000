54d977c1b6a89e61cf59820ae5a2157275dce974
Vanessa Toro <vanetoro84@gmail.com>
2018-05-22 18:07:24 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -6,8 +6,8 @@ class Piglatinizer
   end
 
   def to_pig_latin
-    phrase = text.split()
-    phrase.map do |word|
+    text = text.split()
+    text.map do |word|
       split = word.split(//,2)
       "#{split[1]}#{split[0].downcase}ay "
      end.join
