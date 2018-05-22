2d38e61ac466ff2b5a738052111fcb3c66c8fc25
Vanessa Toro <vanetoro84@gmail.com>
2018-05-22 18:19:18 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -6,7 +6,7 @@ class PigLatinizer
     text = text.split()
     text.map do |word|
       split = word.split(//,2)
-      "#{split[1]}#{split[0].downcase}ay "
+      "#{split[1]}#{split[0].downcase}ay"
      end.join
   end
 
