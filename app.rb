0ee2c9e3ea7c38f20d33f3b8f3be585ff8a310fd
Vanessa Toro <vanetoro84@gmail.com>
2018-05-22 18:08:12 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,6 @@
 require_relative 'config/environment'
+require 'pry'
+
 
 class App < Sinatra::Base
 
@@ -7,6 +9,7 @@ class App < Sinatra::Base
   end
 
   post '/user_input' do
+    binding.pry
     @text = Piglatinizer.new(params[:name])
     @text.to_pig_latin
   end
